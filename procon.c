65ef19c6c524ff56f1fd1c3646d4924e42085757
ISU <wavesinu@gmail.com>
2023-11-26 13:04:48 +0900

    Refactor: procon.c

--- procon.c
+++ procon.c
@@ -1,52 +1,62 @@
 #include "types.h"
 #include "stat.h"
-#include "fcntl.h"
 #include "user.h"
-#include "x86.h"
-#include "semaphore.h"
+#include "fcntl.h"
 
+#define N 10000 // number of items to be produced and consumed
 #define BUFFER_SIZE 1
-#define DATA_SIZE 10000
 
 int buffer[BUFFER_SIZE];
-int in = 0;
-int out = 0;
+int fill_ptr = 0;
+int use_ptr = 0;
+int count = 0;
+
 int pro_counter[2] = {0, 0};
 int con_counter[2] = {0, 0};
 
-int empty;
-int full;
-int mutex;
+int empty, full, mutex;
 
-void *producer(void *param)
+void put(int value)
 {
-    int id = *((int *)param);
-    for (int i = 0; i < DATA_SIZE; i++)
+    buffer[fill_ptr] = value;
+    fill_ptr = (fill_ptr + 1) % BUFFER_SIZE;
+    count++;
+}
+
+int get()
+{
+    int tmp = buffer[use_ptr];
+    use_ptr = (use_ptr + 1) % BUFFER_SIZE;
+    count--;
+    return tmp;
+}
+
+void producer(void *arg)
+{
+    int i;
+    int id = *(int *)arg;
+    for (i = 0; i < N; i++)
     {
         sem_wait(empty);
         sem_wait(mutex);
-
-        buffer[in] = i;
-        in = (in + 1) % BUFFER_SIZE;
+        put(i);
         pro_counter[id]++;
-
         sem_signal(mutex);
         sem_signal(full);
     }
     exit();
 }
 
-void *consumer(void *param)
+void consumer(void *arg)
 {
-    int id = *((int *)param);
-    for (int i = 0; i < DATA_SIZE; i++)
+    int i;
+    int id = *(int *)arg;
+    for (i = 0; i < N; i++)
     {
         sem_wait(full);
         sem_wait(mutex);
-
-        out = (out + 1) % BUFFER_SIZE;
+        get();
         con_counter[id]++;
-
         sem_signal(mutex);
         sem_signal(empty);
     }
@@ -55,23 +65,22 @@ void *consumer(void *param)
 
 int main()
 {
-    int pro[2], con[2];
-    int pro_id[2] = {0, 1}, con_id[2] = {0, 1};
+    int i;
+    int ids[2] = {0, 1};
 
     empty = sem_create(BUFFER_SIZE);
     full = sem_create(0);
     mutex = sem_create(1);
 
-    for (int i = 0; i < 2; i++)
+    for (i = 0; i < 2; i++)
     {
-        pro[i] = hufs_thread_create(producer, &pro_id[i]);
-        con[i] = hufs_thread_create(consumer, &con_id[i]);
+        hufs_thread_create(producer, &ids[i]);
+        hufs_thread_create(consumer, &ids[i]);
     }
 
-    for (int i = 0; i < 2; i++)
+    for (i = 0; i < 4; i++)
     {
-        hufs_thread_join(pro[i]);
-        hufs_thread_join(con[i]);
+        hufs_thread_join(i + 1);
     }
 
     printf(1, "producer (1): %d produced\n", pro_counter[0]);
